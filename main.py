67daea6844ebfabb83f8b61e09f465562931b52c
Wojciech Trybus <wojtryb@gmail.com>
2025-01-06 12:22:30 +0100

    Separate interval and melody exercises

--- main.py
+++ main.py
@@ -11,9 +11,14 @@ from melody_navigator import MelodyNavigator
 
 
 def request_new_tune() -> MelodyNavigator:
-    scale = scale_generators.random_diatonic()
-    # scale = scale_generators.random_chromatic()
+    # scale = scale_generators.random_chromatic(
+    #     lowest_tonic="C2", highest_tonic="C5")
+    # melody = random_step_melody(scale, notes_to_play=2, max_jump=12)
+
+    scale = scale_generators.random_diatonic(
+        lowest_tonic="C2", highest_tonic="C5")
     melody = random_step_melody(scale, notes_to_play=4, max_jump=3)
+
     return MelodyNavigator(melody, instrument.Piano())
 
 
--- scale_generators.py
+++ scale_generators.py
@@ -3,13 +3,15 @@ import random
 from music21.scale import MinorScale, MajorScale, ChromaticScale, ConcreteScale
 
 
-def random_diatonic() -> ConcreteScale:
+def random_diatonic(lowest_tonic: str, highest_tonic: str) -> ConcreteScale:
     scale_type = random.choice((MajorScale, MinorScale))
-    tonic = random.choice(ChromaticScale().getPitches("C2", "C5"))
+    tonic = random.choice(ChromaticScale().getPitches(
+        lowest_tonic, highest_tonic))
     scale = scale_type(tonic)
     return scale
 
 
-def random_chromatic() -> ChromaticScale:
-    tonic = random.choice(ChromaticScale().getPitches("C2", "C5"))
+def random_chromatic(lowest_tonic: str, highest_tonic: str) -> ChromaticScale:
+    tonic = random.choice(ChromaticScale().getPitches(
+        lowest_tonic, highest_tonic))
     return ChromaticScale(tonic)
